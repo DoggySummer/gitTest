8683ad5a49b60ddefede7440a5fbe20e07e9e8de
DoggySummer <91588779+DoggySummer@users.noreply.github.com>
2022-12-30 22:33:04 +0900

    scss test

---
+++ testing/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import './';
+function App() {
+  return (
+    <div className="App">
+      <div className='test'>
+        <ul>
+          <li>dd</li>
+          <li>dd</li>
+
+          <li>dd</li>
+          <li>dd</li>
+
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
